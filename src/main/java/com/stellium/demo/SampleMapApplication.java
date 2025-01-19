ebabd1647ccfbf1943b57fd0439b16e58abe5a13
aetlr <qbuild070@gmail.com>
2025-01-20 06:56:39 +0900

    init

---
+++ src/main/java/com/stellium/demo/SampleMapApplication.java
@@ -0,0 +1,13 @@
+package com.stellium.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMapApplication.class, args);
+	}
+
+}
